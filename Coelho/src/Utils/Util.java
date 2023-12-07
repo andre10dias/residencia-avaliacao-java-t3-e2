4ea5c680bc8a69c22cc339be2cba50931c389f76
Rafaela <rcfbritto@gmail.com>
2023-12-07 10:54:39 -0300

    Metodo na classe Util para formato de data e moeda

--- Coelho/src/Utils/Util.java
+++ Coelho/src/Utils/Util.java
@@ -1,5 +1,11 @@
 package Utils;
 
+import java.text.NumberFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 public class Util {
 	
 	public static int stringToInt(String str) {
@@ -11,5 +17,16 @@ public class Util {
 		
 		return retorno;
 	}
+	public static String formatDate(Date date) {
+        if (date == null) {
+            return "";
+        }
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        return sdf.format(date);
+    }
+	public static String formatCurrency(double amount) {
+        NumberFormat currencyFormat = NumberFormat.getCurrencyInstance(new Locale("pt", "BR"));
+        return currencyFormat.format(amount);
+    }
 	
 }
